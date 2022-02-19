23fe8ca94bfb4a880598c0510b16669497f996d4
AyamNil <87582509+AyamNil@users.noreply.github.com>
2022-02-19 21:50:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Finkha" && b=="21-02-2003")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("Ini Finkha bukan sih?? jawab nya yg bener dong 'F' nya gede");
+  		window.location.href="index.html";
+  	}
+  	
